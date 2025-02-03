410d5fb534721601ce079be9a2ca5c29e24718a5
Lomax <lomaxwebb@yahoo.com>
2025-02-03 17:46:15 -0500

    Add search result item component.

--- search-time-front-end/src/app/search-results-table/search-results-table.component.ts
+++ search-time-front-end/src/app/search-results-table/search-results-table.component.ts
@@ -1,8 +1,10 @@
 
 import { Component } from '@angular/core';
+import { SearchResultItem } from '../search-result-item/search-result-item.component';
 
 @Component({
   selector: 'search-results-table',
+  imports: [SearchResultItem],
   templateUrl: './search-results-table.component.html'
 })
 export class SearchResultsTable {
