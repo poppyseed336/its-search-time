b209e169aa707fe9886e3c6a20976beae0c9baf6
Lomax <lomaxwebb@yahoo.com>
2025-02-05 15:26:41 -0500

    Move dummy data to back end.

--- SearchTimeBackEnd/Controllers/SearchController.cs
+++ SearchTimeBackEnd/Controllers/SearchController.cs
@@ -15,9 +15,83 @@ namespace SearchTimeBackEnd.Controllers
         }
 
         [HttpGet(Name = "Search")]
-        public String Get()
+        public ActionResult<IEnumerable<SearchResultItem>> Get()
         {
-            return "Hello world!";
+            return Ok(GetDummyData());
+        }
+
+        // to-do: Use a service-layer class to return live data from a DB.
+        private List<SearchResultItem> GetDummyData()
+        {
+            return new List<SearchResultItem>
+            {
+                new SearchResultItem
+                {
+                    Id = 1167,
+                    Title = "Guitar",
+                    Color = "blue"
+                },
+                new SearchResultItem
+                {
+                    Id = 9992,
+                    Title = "Video Game",
+                    Color = "green"
+                },
+                new SearchResultItem
+                {
+                    Id = 2424,
+                    Title = "Dinosaur",
+                    Color = "red"
+                },
+                new SearchResultItem
+                {
+                    Id = 6552,
+                    Title = "Dress",
+                    Color = "purple"
+                },
+                new SearchResultItem
+                {
+                    Id = 7777,
+                    Title = "Planet",
+                    Color = "red"
+                },
+                new SearchResultItem
+                {
+                    Id = 1010,
+                    Title = "President",
+                    Color = "orange"
+                },
+                new SearchResultItem
+                {
+                    Id = 1011,
+                    Title = "Chapstick",
+                    Color = "red"
+                },
+                new SearchResultItem
+                {
+                    Id = 3360,
+                    Title = "Food",
+                    Color = "red"
+                },
+                new SearchResultItem
+                {
+                    Id = 1919,
+                    Title = "Beverage",
+                    Color = "green"
+                },
+                new SearchResultItem
+                {
+                    Id = 2123,
+                    Title = "Lifeguard",
+                    Color = "red"
+                },
+                new SearchResultItem
+                {
+                    Id = 6666,
+                    Title = "Tunnel",
+                    Color = "purple"
+                }
+            };
         }
     }
 }
--- search-time-front-end/src/app/services/search.service.ts
+++ search-time-front-end/src/app/services/search.service.ts
@@ -1,6 +1,6 @@
 
 import { Injectable } from '@angular/core';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import axios from 'axios';
 import { DataService } from './data.service';
 import { Item } from '../models/item.model';
@@ -18,30 +18,14 @@ export class SearchService {
   constructor(private dataService: DataService) { }
 
   fetchSearchResults() {
-    // to-do: For now this mocks a remote API call. Later we'll add an actual API call and make this function async.
-    let results = [
-      { id: 1167, title: 'Guitar', color: 'blue' } as Item,
-      { id: 9992, title: 'Video Game', color: 'green' } as Item,
-      { id: 2424, title: 'Dinosaur', color: 'red' } as Item,
-      { id: 6552, title: 'Dress', color: 'purple' } as Item,
-      { id: 7777, title: 'Planet', color: 'red' } as Item,
-      { id: 1010, title: 'President', color: 'orange' } as Item,
-      { id: 1011, title: 'Chapstick', color: 'red' } as Item,
-      { id: 3360, title: 'Food', color: 'red' } as Item,
-      { id: 1919, title: 'Beverage', color: 'green' } as Item,
-      { id: 2123, title: 'lifeguard', color: 'red' } as Item,
-      { id: 6666, title: 'tunnel', color: 'purple' } as Item
-    ];
 
     axios.get(this.searchUrl)
       .then((response) => {
-        console.log(JSON.stringify(response.data));
+        this.dataService.setSearchResults(response.data);
       })
       .catch((error) => {
         console.error('Error executing search: ', error);
         throw error;
       });
-
-    this.dataService.setSearchResults(results);
   }
 }
