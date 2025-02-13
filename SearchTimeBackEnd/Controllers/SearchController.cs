31a537047b67ec0ff55715a77d2acd005ad54b07
Lomax <lomaxwebb@yahoo.com>
2025-02-13 01:34:31 -0500

    Remove dummy data.

--- SearchTimeBackEnd/Controllers/SearchController.cs
+++ SearchTimeBackEnd/Controllers/SearchController.cs
@@ -21,37 +21,9 @@ namespace SearchTimeBackEnd.Controllers
         [HttpGet(Name = "Search")]
         public async Task<ActionResult<SearchResultsViewModel>> Get(int pageNumber = 1, string title = "", string color = "")
         {
-            // to-do: Implement this for the actual, live DB results
             SearchResultsViewModel results = await _searchService.GetSearchResults(pageNumber, title, color);
 
-            // to-do: remove this dummy data method and use live DB results
-            //SearchResultsViewModel results = GetDummyData(title, color);
-
             return Ok(results);
-        }
-
-        // to-do: remove this dummy data method and use live DB results
-        private SearchResultsViewModel GetDummyData(string title, string color)
-        {
-            var dummySearchResults = new List<Models.Entities.SearchResultItem>()
-                {
-                    new Models.Entities.SearchResultItem() { Id = 1, Title = title, Color = color },
-                    new Models.Entities.SearchResultItem() { Id = 3, Title = title, Color = color },
-                    new Models.Entities.SearchResultItem() { Id = 45, Title = title, Color = color },
-                    new Models.Entities.SearchResultItem() { Id = 50, Title = title, Color = color },
-                    new Models.Entities.SearchResultItem() { Id = 51, Title = title, Color = color },
-                    new Models.Entities.SearchResultItem() { Id = 52, Title = title, Color = color },
-                    new Models.Entities.SearchResultItem() { Id = 54, Title = title, Color = color },
-                    new Models.Entities.SearchResultItem() { Id = 59, Title = title, Color = color },
-                    new Models.Entities.SearchResultItem() { Id = 60, Title = title, Color = color },
-                    new Models.Entities.SearchResultItem() { Id = 64, Title = title, Color = color }
-                };
-
-            return new SearchResultsViewModel()
-            {
-                NumberOfResults = 177,
-                SearchResults = dummySearchResults
-            };
         }
     }
 }
